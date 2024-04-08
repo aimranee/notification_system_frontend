ace512500bc5ef143f2ff2e20ed8d7144e614382
aimrane <aimrane.essakhi@gmail.com>
2024-04-08 15:54:11 +0000

    create layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,36 @@
 import "@/styles/globals.css";
+import { Sidebar } from "@/components/layouts/sidebar";
+import { Header } from "@/components/layouts/header";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
-import Home from "./page";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
-export default function RootLayout() {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
+    <html lang="en">
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}
-      >
-        {/* Rendering the Home component */}
-        <Home />
+        >
+        <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+          <Sidebar />
+          <div className="flex flex-col">
+            <Header />
+            <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+              {children}
+            </main>
+          </div>
+        </div>
       </body>
     </html>
   );
