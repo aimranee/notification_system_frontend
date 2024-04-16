3b3bbcc22653d743f8551b91da51df5fab9c681a
aimrane <aimrane.essakhi@gmail.com>
2024-04-16 12:31:22 +0100

    Event page

--- src/pages/event/index.tsx
+++ src/pages/event/index.tsx
@@ -1,26 +1,22 @@
 import React, { ReactElement } from "react";
-import { Button } from "@/components/ui/button";
 import Layout from "@/components/layouts/Layout";
+import EventCreate from "@/components/event/EventCreate";
+import EventList from "@/components/event/EventList";
 
 const Event = ({}) => {
   return (
     <>
-      <div className="flex items-center">
-        <h1 className="text-lg font-semibold md:text-2xl">Events</h1>
+      <div className="flex h-14 items-center">
+        <div className="w-full flex-1">
+          <h1 className="text-lg font-semibold md:text-2xl">Events</h1>
+        </div>
+        <EventCreate />
       </div>
       <div
-        className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
+        className="flex flex-col rounded-lg border border-dashed shadow-sm p-4"
         x-chunk="dashboard-02-chunk-1"
       >
-        <div className="flex flex-col items-center gap-1 text-center">
-          <h3 className="text-2xl font-bold tracking-tight">
-            You have no events
-          </h3>
-          <p className="text-sm text-muted-foreground">
-            You can start selling as soon as you add a event.
-          </p>
-          <Button className="mt-4">Add Product</Button>
-        </div>
+        <EventList />
       </div>
     </>
   );
