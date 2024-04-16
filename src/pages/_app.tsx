1f94edec5d28472328ed66e248918180c34674b2
aimrane <aimrane.essakhi@gmail.com>
2024-04-16 18:21:18 +0100

    integrate react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,8 @@ import type { AppProps } from "next/app";
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import HeadWrapper from "@/components/HeadWrapper";
-import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -13,12 +14,17 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return getLayout(
-    <HeadWrapper>
-      <Component {...pageProps} />
-    </HeadWrapper>
+    <QueryClientProvider client={queryClient}>
+      <HeadWrapper>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Component {...pageProps} />
+      </HeadWrapper>
+    </QueryClientProvider>
   );
 }
