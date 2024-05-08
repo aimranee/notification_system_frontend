8f84bed4e1d6dd6d0dd44848d12f4d947da5556b
aimrane <aimrane.essakhi@gmail.com>
2024-05-08 10:53:58 +0100

    update configuration style

---
+++ src/config/sites.ts
@@ -0,0 +1,13 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Next.js",
+  description:
+    "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
+  mainNav: [
+    {
+      title: "Dashboard",
+      href: "/",
+    },
+  ],
+};
