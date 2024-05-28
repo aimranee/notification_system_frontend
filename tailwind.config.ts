fc2dc85c98c33fddc1d9ed521be7d4d8b1097384
aimrane <aimrane.essakhi@gmail.com>
2024-05-28 17:24:21 +0100

    update envirement because i had error on setup of the project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,15 @@
-const { fontFamily } = require("tailwindcss/defaultTheme");
-
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
   prefix: "",
   theme: {
-    fontFamily: {
-      sans: ["var(--font-sans)", ...fontFamily.sans],
-    },
     container: {
       center: true,
       padding: "2rem",
@@ -80,6 +75,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+} satisfies Config
 
-export default config;
+export default config
